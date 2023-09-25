e94b76162612b9ec178c5da5ba46a77479585a8a
KyEBell <kyleevanbell@gmail.com>
2023-09-25 16:09:39 -0400

    updated colors and cleaned up logs ready to pull

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,9 +3,9 @@ import Card from '../UI/Card';
 const UsersList = (props) => {
   const listItem = props.users.map((element) => {
     return (
-      <li key={element.id} className={classes.users}>
-        {element.name} ({element.age} years old)
-      </li>
+        <li key={element.id} className={classes.users}>
+          {element.name} ({element.age} years old)
+        </li>
     );
   });
   return (
